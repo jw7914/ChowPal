b3d518c5a5525c39b543e05f87b18524424832b5
jw7914 <jw7914@nyu.edu>
2025-02-11 18:57:06 -0500

    firebase config creds added

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,28 @@
 from typing import Union
-
-from fastapi import FastAPI
-
+from firebase_admin import credentials, auth
+import firebase_admin
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException, Query
+load_dotenv()
 app = FastAPI()
+
+
+firebase_config = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+}
+creds = credentials.Certificate(firebase_config)
+firebase_admin.initialize_app(creds)
 
 
 @app.get("/")
@@ -13,4 +33,12 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
-    
+
+
+@app.get("/firebase")
+def authenticate_user(email: str = Query(..., description="User email to authenticate")):
+    try:
+        user = auth.get_user_by_email(email)
+        return {"message": "User found", "uid": user.uid}
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=f"Error finding user: {e}")
